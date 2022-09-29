001d5cbbcd2f61d25df97f572a18356397c45340
Mr Robot <mr.robot.morph@gmail.com>
2022-09-29 16:01:22 +0200

    intermediate save

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,25 @@
 let element = document.querySelector(".menu");
 let button = document.querySelector(".button");
 let body = document.querySelector('body');
+let lang = document.querySelector('.lang');
 
 /* Closed and open burger menu */
 button.addEventListener("click", () => {
-  element.classList.toggle("close-open-menu");
+   element.classList.toggle("close-open-menu");
 })
+
+/* Add languages menu */
+button.addEventListener("click", () => {
+  lang.classList.toggle("add-lang");
+})
+
 
 /* Stop scroll */
 button.addEventListener("click", () => {
     body.classList.toggle("stop-scroll")
+})
+
+
+button.addEventListener("click", () => {
+  button.classList.toggle("button-click-top");
 })
